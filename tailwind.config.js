4abdea0c87f0a42b908a5240c8e261eb7c61c827
Hamzat Victor <oluwaborihamzat@gmail.com>
2023-11-21 11:06:22 +0100

    feat: extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6D33CC",
+      },
+      fontFamily: {
+        rubric: ["Rubric", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
